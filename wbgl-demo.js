046ca208d09160395f466003229d6fc7facd9dc8
M4rchy-S <alex12345suslik@gmail.com>
2025-08-13 11:41:03 +0300

    Check 1

---
+++ wbgl-demo.js
@@ -0,0 +1,236 @@
+
+var vsSource = `
+  attribute vec2 a_position;
+  attribute vec2 a_texCoord;
+
+  uniform vec2 u_resolution;
+
+  varying vec2 v_texCoord;
+  
+
+  void main() {
+    // convert the position from pixels to 0.0 to 1.0
+    vec2 zeroToOne = a_position / u_resolution;
+ 
+    // convert from 0->1 to 0->2
+    vec2 zeroToTwo = zeroToOne * 2.0;
+ 
+    // convert from 0->2 to -1->+1 (clip space)
+    vec2 clipSpace = zeroToTwo - 1.0;
+ 
+    gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+    v_texCoord = a_texCoord;
+  }
+`
+var frSource = `
+  precision mediump float;
+
+  uniform vec4 u_color;
+  
+  varying vec2 v_texCoord;
+  uniform sampler2D u_image;
+
+  void main() {
+    // gl_FragColor = u_color;
+    gl_FragColor = texture2D(u_image, v_texCoord);
+  }
+
+`
+
+function createShader(gl, type, source) {
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+}
+
+function render(image) {
+  var canvas = document.querySelector('#c');
+  var gl = canvas.getContext('webgl');
+  if (!gl) {
+    return;
+  }
+
+  // Get the strings for our GLSL shaders
+
+  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vsSource);
+  var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, frSource);
+
+  var program = createProgram(gl, vertexShader, fragmentShader);
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  gl.useProgram(program);
+
+  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+  var positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  setRectangle(gl, 140, 200, image.width * 3, image.height * 3);
+
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  var size = 2;          // 2 components per iteration
+  var type = gl.FLOAT;   // the data is 32bit floats
+  var normalize = false; // don't normalize the data
+  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0;        // start at the beginning of the buffer
+  gl.vertexAttribPointer(
+    positionAttributeLocation, size, type, normalize, stride, offset);
+
+  var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+  var colorUniformLocation = gl.getUniformLocation(program, "u_color");
+  gl.uniform4f(colorUniformLocation, 0.0, 1.0, 0.0, 1.0);
+
+  var texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
+
+  var texCoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    0.0,  0.0,
+    1.0,  0.0,
+    0.0,  1.0,
+    0.0,  1.0,
+    1.0,  0.0,
+    1.0,  1.0
+
+    // 0.0, 0.0,
+    // 0.5, 0.0,
+    // 0.0, 0.5,
+
+    // 0.0, 0.5,
+    // 0.5, 0.0,
+    // 0.5, 0.5,
+
+  ]), gl.STATIC_DRAW);
+
+  gl.enableVertexAttribArray(texCoordLocation);
+  gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+
+  var texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+ 
+  // Set the parameters so we can render any size image.
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+ 
+  // Upload the image into the texture.
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+
+
+
+  // draw
+  var primitiveType = gl.TRIANGLES;
+  var offset = 0;
+  var count = 6;
+  gl.drawArrays(primitiveType, offset, count);
+
+}
+
+
+function main() {
+  var image = new Image();
+  image.src = "font.png";
+  image.onload = function() {
+    render(image);
+  }
+}
+ 
+
+main();
+
+
+function setRectangle(gl, x, y, width, height) {
+  var x1 = x;
+  var x2 = x + width;
+  var y1 = y;
+  var y2 = y + height;
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    x1, y1,
+    x2, y1,
+    x1, y2,
+    x1, y2,
+    x2, y1,
+    x2, y2,
+  ]), gl.STATIC_DRAW);
+}
+
+var fontInfo = {
+  letterHeight: 8,
+  spaceWidth: 8,
+  spacing: -1,
+  textureWidth: 64,
+  textureHeight: 40,
+  glyphInfos: {
+    'a': { x: 0, y: 0, width: 8, },
+    'b': { x: 8, y: 0, width: 8, },
+    'c': { x: 16, y: 0, width: 8, },
+    'd': { x: 24, y: 0, width: 8, },
+    'e': { x: 32, y: 0, width: 8, },
+    'f': { x: 40, y: 0, width: 8, },
+    'g': { x: 48, y: 0, width: 8, },
+    'h': { x: 56, y: 0, width: 8, },
+    'i': { x: 0, y: 8, width: 8, },
+    'j': { x: 8, y: 8, width: 8, },
+    'k': { x: 16, y: 8, width: 8, },
+    'l': { x: 24, y: 8, width: 8, },
+    'm': { x: 32, y: 8, width: 8, },
+    'n': { x: 40, y: 8, width: 8, },
+    'o': { x: 48, y: 8, width: 8, },
+    'p': { x: 56, y: 8, width: 8, },
+    'q': { x: 0, y: 16, width: 8, },
+    'r': { x: 8, y: 16, width: 8, },
+    's': { x: 16, y: 16, width: 8, },
+    't': { x: 24, y: 16, width: 8, },
+    'u': { x: 32, y: 16, width: 8, },
+    'v': { x: 40, y: 16, width: 8, },
+    'w': { x: 48, y: 16, width: 8, },
+    'x': { x: 56, y: 16, width: 8, },
+    'y': { x: 0, y: 24, width: 8, },
+    'z': { x: 8, y: 24, width: 8, },
+    '0': { x: 16, y: 24, width: 8, },
+    '1': { x: 24, y: 24, width: 8, },
+    '2': { x: 32, y: 24, width: 8, },
+    '3': { x: 40, y: 24, width: 8, },
+    '4': { x: 48, y: 24, width: 8, },
+    '5': { x: 56, y: 24, width: 8, },
+    '6': { x: 0, y: 32, width: 8, },
+    '7': { x: 8, y: 32, width: 8, },
+    '8': { x: 16, y: 32, width: 8, },
+    '9': { x: 24, y: 32, width: 8, },
+    '-': { x: 32, y: 32, width: 8, },
+    '*': { x: 40, y: 32, width: 8, },
+    '!': { x: 48, y: 32, width: 8, },
+    '?': { x: 56, y: 32, width: 8, },
+  },
+};
